48456d4e268f00180e950f07f28026b95f6a9174
Andrea <linckandrea01@gmail.com>
2020-07-30 00:57:41 +0200

    fixup

--- anykernel.sh
+++ anykernel.sh
@@ -34,6 +34,8 @@ ramdisk_compression=auto;
 set_perm_recursive 0 0 755 644 $ramdisk/*;
 set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
 
+## AnyKernel install
+/tmp/anykernel/tools/busybox mount -o rw,remount -t auto /system;
 
 ## AnyKernel install
 dump_boot;
@@ -41,21 +43,19 @@ dump_boot;
 # begin ramdisk changes
 
 # init.target.rc
-backup_file init.target.rc;
-replace_string init.target.rc "service mpdecision /system/vendor/bin/mpdecision --avg_comp --Nw=1:1.99 --Nw=2:2.99 --Nw=3:3.99 --Tw=2:140 --Tw=3:140 --Ts=2:190 --Ts=3:190" "service mpdecision /system/vendor/bin/mpdecision --avg_comp" "service mpdecision /system/vendor/bin/mpdecision --avg_comp --Nw=1:1.99 --Nw=2:2.99 --Nw=3:3.99 --Tw=2:140 --Tw=3:140 --Ts=2:190 --Ts=3:190"
-ui_print "- Applying pop kernel init patch in ramdisk"
-insert_line init.qcom.rc "init.pop.rc" after "import init.target.rc" "import init.pop.rc";
+replace_string "/system/vendor/etc/init/hw/init.target.rc" "service mpdecision /system/vendor/bin/mpdecision --avg_comp --Nw=1:1.99 --Nw=2:2.99 --Nw=3:3.99 --Tw=2:140 --Tw=3:140 --Ts=2:190 --Ts=3:190" "service mpdecision /system/vendor/bin/mpdecision --avg_comp" "service mpdecision /system/vendor/bin/mpdecision --avg_comp --Nw=1:1.99 --Nw=2:2.99 --Nw=3:3.99 --Tw=2:140 --Tw=3:140 --Ts=2:190 --Ts=3:190"
 
 # init.qcom.power.rc
-backup_file init.qcom.rc;
-replace_string init.qcom.power.rc "#start mpdecision" "start mpdecision" "#start mpdecision";
-remove_line init.qcom.rc "write /sys/module/cpu_boost/parameters/boost_ms 20";
-remove_line init.qcom.rc "write /sys/module/cpu_boost/parameters/sync_threshold 1728000";
-remove_line init.qcom.rc "write /sys/module/cpu_boost/parameters/input_boost_freq 1497600";
-remove_line init.qcom.rc "write /sys/module/cpu_boost/parameters/input_boost_ms 40";
+remove_line "/system/vendor/etc/init/hw/init.qcom.rc" "start mpdecision";
+remove_line "/system/vendor/etc/init/hw/init.qcom.rc" "write /sys/module/cpu_boost/parameters/boost_ms 20";
+remove_line "/system/vendor/etc/init/hw/init.qcom.rc" "write /sys/module/cpu_boost/parameters/sync_threshold 1728000";
+remove_line "/system/vendor/etc/init/hw/init.qcom.rc" "write /sys/module/cpu_boost/parameters/input_boost_freq 1497600";
+remove_line "/system/vendor/etc/init/hw/init.qcom.rc" "write /sys/module/cpu_boost/parameters/input_boost_ms 40";
+replace_string "/system/vendor/etc/init/hw/init.qcom.rc" "268800" "300000" "268800" "global";
 
 # end ramdisk changes
 
 write_boot;
+/tmp/anykernel/tools/busybox mount -o ro,remount -t auto /system;
 ## end install
 
