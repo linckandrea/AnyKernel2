eb90b9b64b6881f8765d15bc37532d1f12d39b32
Andrea <linckandrea01@gmail.com>
2020-07-29 13:11:35 +0200

    victara-fy

--- anykernel.sh
+++ anykernel.sh
@@ -4,23 +4,22 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=ExampleKernel by osm0sis @ xda-developers
+kernel.string=Pop kernel by linckandrea @ xda-developers
 do.devicecheck=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=maguro
-device.name2=toro
-device.name3=toroplus
-device.name4=tuna
-device.name5=
+device.name1=xt1092
+device.name2=xt1095
+device.name3=xt1097
+device.name4=victara
 supported.versions=
 supported.patchlevels=
 '; } # end properties
 
 # shell variables
-block=/dev/block/platform/omap/omap_hsmmc.0/by-name/boot;
+block=/dev/block/platform/msm_sdcc.1/by-name/boot;
 is_slot_device=0;
 ramdisk_compression=auto;
 
@@ -41,21 +40,19 @@ dump_boot;
 
 # begin ramdisk changes
 
-# init.rc
-backup_file init.rc;
-replace_string init.rc "cpuctl cpu,timer_slack" "mount cgroup none /dev/cpuctl cpu" "mount cgroup none /dev/cpuctl cpu,timer_slack";
+# init.target.rc
+backup_file init.target.rc;
+replace_string init.target.rc "service mpdecision /system/vendor/bin/mpdecision --avg_comp --Nw=1:1.99 --Nw=2:2.99 --Nw=3:3.99 --Tw=2:140 --Tw=3:140 --Ts=2:190 --Ts=3:190" "service mpdecision /system/vendor/bin/mpdecision --avg_comp" "service mpdecision /system/vendor/bin/mpdecision --avg_comp --Nw=1:1.99 --Nw=2:2.99 --Nw=3:3.99 --Tw=2:140 --Tw=3:140 --Ts=2:190 --Ts=3:190"
+ui_print "- Applying pop kernel init patch in ramdisk"
+insert_line init.qcom.rc "init.pop.rc" after "import init.target.rc" "import init.pop.rc";
 
-# init.tuna.rc
-backup_file init.tuna.rc;
-insert_line init.tuna.rc "nodiratime barrier=0" after "mount_all /fstab.tuna" "\tmount ext4 /dev/block/platform/omap/omap_hsmmc.0/by-name/userdata /data remount nosuid nodev noatime nodiratime barrier=0";
-append_file init.tuna.rc "bootscript" init.tuna;
-
-# fstab.tuna
-backup_file fstab.tuna;
-patch_fstab fstab.tuna /system ext4 options "noatime,barrier=1" "noatime,nodiratime,barrier=0";
-patch_fstab fstab.tuna /cache ext4 options "barrier=1" "barrier=0,nomblk_io_submit";
-patch_fstab fstab.tuna /data ext4 options "data=ordered" "nomblk_io_submit,data=writeback";
-append_file fstab.tuna "usbdisk" fstab;
+# init.qcom.power.rc
+backup_file init.qcom.rc;
+replace_string init.qcom.power.rc "#start mpdecision" "start mpdecision" "#start mpdecision";
+remove_line init.qcom.rc "write /sys/module/cpu_boost/parameters/boost_ms 20";
+remove_line init.qcom.rc "write /sys/module/cpu_boost/parameters/sync_threshold 1728000";
+remove_line init.qcom.rc "write /sys/module/cpu_boost/parameters/input_boost_freq 1497600";
+remove_line init.qcom.rc "write /sys/module/cpu_boost/parameters/input_boost_ms 40";
 
 # end ramdisk changes
 
